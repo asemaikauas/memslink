9949d38fb997dd3f4eabfa8a6cb26d49eaec0be2
asemaikauas <108888800+asemaikauas@users.noreply.github.com>
2025-04-28 00:42:10 +0500

    Delete postgres_config.py

--- postgres_config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-POSTGRES_USER = os.environ.get('POSTGRES_USER', 'postgres')
-POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'postgres')
-POSTGRES_HOST = os.environ.get('POSTGRES_HOST', 'localhost')
-POSTGRES_DB = os.environ.get('POSTGRES_DB', 'memeswipe')
-
-SQLALCHEMY_DATABASE_URI = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
